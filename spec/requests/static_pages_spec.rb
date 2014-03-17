edf60d4e8d7536f0dc3f27491373d909ecd8eb38
Tasha Lawson <tlawson@chicagotechacademy.org>
2014-03-17 13:17:15 -0500

    static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,4 +8,10 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
   end
+    it "should have the contene Help page " do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
 end
